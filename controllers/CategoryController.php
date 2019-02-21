257348863f12a6c894e6733799fe1ee418d37f76
sevak07 <sevak.aghajanyan.upwork@gmail.com>
2019-02-21 17:40:18 +0400

    default route, extending appcontroller

---
+++ controllers/CategoryController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace app\controllers;
+
+use app\models\Category;
+use app\models\Product;
+use Yii;
+
+class CategoryController extends AppController{
+
+	public function actionIndex(){
+		$hits = Product::find()->where(["hit" => '1'])->limit(6)->all();
+		debug($hits);
+		return $this->render('index', compact('hits'));
+	}
+}
