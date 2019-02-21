b3ce3fc68da81a21c8a7936112f821917d54013c
sevak07 <sevak.aghajanyan.upwork@gmail.com>
2019-02-21 18:09:03 +0400

    html::img  and add to view

--- controllers/CategoryController.php
+++ controllers/CategoryController.php
@@ -10,7 +10,7 @@ class CategoryController extends AppController{
 
 	public function actionIndex(){
 		$hits = Product::find()->where(["hit" => '1'])->limit(6)->all();
-		debug($hits);
+		// debug($hits);
 		return $this->render('index', compact('hits'));
 	}
 }
--- views/category/index.php
+++ views/category/index.php
@@ -2,6 +2,8 @@
 
 /* @var $this yii\web\View */
 
+use yii\helpers\Html;
+
 $this->title = 'My Yii Application';
 ?>
 <div class="site-index">
@@ -112,24 +114,26 @@ $this->title = 'My Yii Application';
             </div>
             
             <div class="col-sm-9 padding-right">
+                <?php if( !empty($hits) ) : ?>
                 <div class="features_items"><!--features_items-->
                     <h2 class="title text-center">Features Items</h2>
+                    <?php foreach($hits as $hit): ?>
                     <div class="col-sm-4">
                         <div class="product-image-wrapper">
                             <div class="single-products">
                                     <div class="productinfo text-center">
-                                        <img src="images/home/product1.jpg" alt="" />
-                                        <h2>$56</h2>
-                                        <p>Easy Polo Black Edition</p>
+                                        <?= Html::img("@web/images/products/{$hit->img}", ['alt' => $hit->name])?>
+                                        <h2>$<?= $hit->price ?></h2>
+                                        <p><?= $hit->name ?></p>
                                         <a href="#" class="btn btn-default add-to-cart"><i class="fa fa-shopping-cart"></i>Add to cart</a>
                                     </div>
-                                    <div class="product-overlay">
+                                    <!-- <div class="product-overlay">
                                         <div class="overlay-content">
-                                            <h2>$56</h2>
-                                            <p>Easy Polo Black Edition</p>
+                                            <h2>$<?= $hit->price ?></h2>
+                                            <p><?= $hit->name ?></p>
                                             <a href="#" class="btn btn-default add-to-cart"><i class="fa fa-shopping-cart"></i>Add to cart</a>
                                         </div>
-                                    </div>
+                                    </div> -->
                             </div>
                             <div class="choose">
                                 <ul class="nav nav-pills nav-justified">
@@ -138,10 +142,11 @@ $this->title = 'My Yii Application';
                                 </ul>
                             </div>
                         </div>
-                    </div>
-                    
-                    
+                    </div>   
+                    <?php endforeach; ?>                
                 </div><!--features_items-->
+
+                <?php endif; ?>
                 
                 <div class="category-tab"><!--category-tab-->
                     <div class="col-sm-12">
