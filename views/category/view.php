309f05e408bf856439759f8f91935bcef6ea0f96
sevak07 <sevak.aghajanyan.upwork@gmail.com>
2019-02-22 15:27:52 +0400

    Product nice url

--- views/category/view.php
+++ views/category/view.php
@@ -63,10 +63,10 @@ use yii\helpers\Html;
 									<div class="productinfo text-center">
 										<?= Html::img("@web/images/products/{$product->img}", ['alt' => $product->name])?>
 										<h2>$<?= $product->price ?></h2>
-										<p><?= $product->name ?></p>
+										<p><a href="<?= \yii\helpers\Url::to(['product/view', 'id' => $product->id ])?>"><?= $product->name ?></a></p>
 										<a href="#" class="btn btn-default add-to-cart"><i class="fa fa-shopping-cart"></i>Add to cart</a>
-									</div><!-- 
-									<div class="product-overlay">
+									</div>
+									<!-- <div class="product-overlay">
 										<div class="overlay-content">
 											<h2>$56</h2>
 											<p>Easy Polo Black Edition</p>
