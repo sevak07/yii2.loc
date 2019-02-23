fd3108ddaaa0d5333777030340849f0320583592
sevak07 <sevak.aghajanyan.upwork@gmail.com>
2019-02-23 20:55:21 +0400

    Product hits and metas

--- controllers/ProductController.php
+++ controllers/ProductController.php
@@ -11,8 +11,10 @@ class ProductController extends AppController {
 	public function actionView($id){
 		$id = Yii::$app->request->get('id');
 		$product = Product::findOne($id);
+		$this->setMeta(' E-SHOPPER | '. $product->name, $product->keywords, $product->description);
 		// $product = Product::find()->with('category')->where(['id' => $id])->limit(1)->one();
-		return $this->render('view', compact('product'));
+		$hits = Product::find()->where(["hit" => '1'])->limit(6)->all();
+		return $this->render('view', compact('product', 'hits'));
 	}
 
 }
