06d7296a0df20d0a412ff722bde35f340b545b88
sevak07 <sevak.aghajanyan.upwork@gmail.com>
2019-02-26 20:47:02 +0400

    Save order_items

--- controllers/CartController.php
+++ controllers/CartController.php
@@ -65,11 +65,28 @@ class CartController extends AppController {
 			$order->qty = $session['cart.qty'];
 			$order->sum = $session['cart.sum'];
 			if($order->save()){
+				$this->saveOrderItems($session['cart'], $order->id);
 				Yii::$app->session->setFlash('success', 'Your order was accepted');
+				$session->remove('cart');
+				$session->remove('cart.qty');
+				$session->remove('cart.sum');
 			} else {
 				Yii::$app->session->setFlash('error', 'Error in order');
 			}
 		}
 		return $this->render('view', compact('session', 'order'));
+	}
+
+	protected function saveOrderItems($items, $order_id){
+		foreach($items as $id => $item) {
+			$order_items = new OrderItems();
+			$order_items->order_id = $order_id;
+			$order_items->product_id = $id;
+			$order_items->name = $item['name'];
+			$order_items->price = $item['price'];
+			$order_items->qty_item = $item['qty'];
+			$order_items->sum_item = $item['qty'] * $item['price'];
+			$order_items->save();
+		}
 	}
 }
