95027e0405def213ae4d8bf618c61b1be15f512e
sevak07 <sevak.aghajanyan.upwork@gmail.com>
2019-02-27 08:57:33 +0400

    Admin module with gii

--- config/web.php
+++ config/web.php
@@ -8,6 +8,11 @@ $config = [
     'basePath' => dirname(__DIR__),
     'bootstrap' => ['log'],
     'defaultRoute' => 'category/index',
+    'modules' => [
+        'admin' => [
+            'class' => 'app\modules\admin\Module',
+        ],
+    ],
     'aliases' => [
         '@bower' => '@vendor/bower-asset',
         '@npm'   => '@vendor/npm-asset',
